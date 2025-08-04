3f502b78e4020951b8d14efed1a21fafdba88c10
somjairs8542-pixel <somjairs8542@gmail.com>
2025-08-04 13:32:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// script.js
+import { db } from "./firebase-config.js";
+import { ref, set } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
 
+set(ref(db, 'test'), {
+  message: "เชื่อม Firebase สำเร็จ"
+});
