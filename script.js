96fe0dcd79c2b763791af9560c74affdf77bb40b
somjairs8542-pixel <somjairs8542@gmail.com>
2025-08-04 14:28:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-// script.js
-import { db, ref, set } from "./firebase-database.js";
+// 1. import firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
 
-// ทดสอบส่งข้อมูลเข้า Firebase
-function sendTestData() {
-  set(ref(db, 'test/'), {
-    message: "ทดสอบการเชื่อมต่อสำเร็จ"
-  }).then(() => {
-    alert("ส่งข้อมูลสำเร็จ!");
-  }).catch((error) => {
-    console.error("ส่งข้อมูลล้มเหลว:", error);
-  });
-}
-
-// เรียก function เมื่อโหลดเว็บ
-window.onload = () => {
-  sendTestData();
+// 2. config Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyB0_WAvqnepPH2slauRyXh7y14uhvvJ2rM",
+  authDomain: "mungkorn888-72072.firebaseapp.com",
+  databaseURL: "https://mungkorn888-72072-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mungkorn888-72072",
+  storageBucket: "mungkorn888-72072.appspot.com",
+  messagingSenderId: "15500489348",
+  appId: "1:15500489348:web:3cee0ae802bc04eb5c48d1",
+  measurementId: "G-VH5Y1T4PNT"
 };
+
+// 3. initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// 4. ส่งข้อมูลทดสอบ
+set(ref(db, 'test'), {
+  message: 'เชื่อม Firebase สำเร็จ!'
+});
