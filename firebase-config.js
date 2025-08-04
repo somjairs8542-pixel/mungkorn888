9a6f81871d7cc1f7f3f1aeae729416ce1bcc9df6
somjairs8542-pixel <somjairs8542@gmail.com>
2025-08-04 14:21:26 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,18 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
+// firebase-database.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB0_WAvqnepPH2slauRyXh7y14uhvvJ2rM",
   authDomain: "mungkorn888-72072.firebaseapp.com",
   databaseURL: "https://mungkorn888-72072-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "mungkorn888-72072",
-  storageBucket: "mungkorn888-72072.firebasestorage.app",
+  storageBucket: "mungkorn888-72072.appspot.com",
   messagingSenderId: "15500489348",
   appId: "1:15500489348:web:3cee0ae802bc04eb5c48d1",
   measurementId: "G-VH5Y1T4PNT"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db };
+export const db = getDatabase(app);
+export { ref, set };
